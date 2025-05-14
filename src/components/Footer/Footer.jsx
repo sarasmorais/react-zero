4bc7795be9616d6491228de7ac67fa64e8aa58f8
sarasmorais <sarasouza.m@gmail.com>
2025-05-14 16:12:48 -0300

    add footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,31 @@
 import "./Footer.css";
 
 export default function Footer(){
+    <footer className="footer" id="contact">
+        <div className="footer-content">
+            <div className="footer-section">
+                <h3>Lorem ipsum dolor sit amet.</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae!</p>
+            </div>
+        </div>
+
+        <div className="footer-section">
+            <h3>Lorem ipsum dolor sit.</h3>
+            <p>Email: email@hotmail.com</p>
+            <p>Phone: 85 9999-9999</p>
+        </div>
+
+        <div className="footer-section">
+            <h3>Lorem dolor sit.</h3>
+            <div>
+                <a href="#">Instagram</a>
+                <a href="#">Orkut</a>
+                <a href="#">MSN</a>
+            </div>
+        </div>
+        <div className="footer-button">
+            <button>&copy; 2025 - Todos os direitos reservados as vozes da minha cabeça</button>
+        </div>
+    </footer>
     
 }
