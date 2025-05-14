cc558d6114252d816655c3918cbd2bbe8bd4606f
sarasmorais <sarasouza.m@gmail.com>
2025-05-14 16:05:17 -0300

    add features

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -1,5 +1,35 @@
 import "./Features.css";
 
 export default function Features(){
-   
+    const featuresList = [
+        {
+            id: 1,
+            title:"Componentes básicos",
+            description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero, cumque!"
+        },
+        {
+            id: 2,
+            title:"Componentes básicos2",
+            description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero, cumque!"
+        },
+        {
+            id: 3,
+            title:"Componentes básicos3",
+            description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero, cumque!"
+        },
+    ]
+
+    return(
+        <section className="features">
+            <h2>Chaves das Features</h2>
+            <div className="features-grid">
+                {featuresList.map((feature)=>{
+                    <div className="features-card" key={feature.id}>
+                    <h3>{feature.title}</h3>
+                    <p>{feature.description}</p>
+                </div>
+                })}
+            </div>
+        </section>
+    )
 }
