48eab0d2abac859dd19072610e1874b6485bdbe5
sarasmorais <sarasouza.m@gmail.com>
2025-05-14 15:52:46 -0300

    att hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,17 @@
 import "./Hero.css";
 
 export default function Hero(){
+    <section className="hero" id="home">
+        <div className="hero-content">
+            <h2> Bem vindo ao mundo de React</h2>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum excepturi hic similique, eos fugit dolorem.
+            </p>
+            <button className="cta-button">
+                Iniciar estudos
+            </button>
+
+        </div>
+    </section>
     
 }
